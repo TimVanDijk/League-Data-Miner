eeb25373cdb6bde43968f3881cc5aa4fe4fa9e8e
Tim van Dijk <tim.vandijk96@gmail.com>
2015-12-05 15:12:10 +0100

    Adds message for error code 403

--- ErrorInfo.py
+++ ErrorInfo.py
@@ -1,6 +1,7 @@
 ERROR = {
     '400' : 'Bad request',
     '401' : 'Unauthorized',
+    '403' : 'Forbidden',
     '404' : 'No results found for specified input',
     '422' : 'Summoner has an entry, but hasn\'t played since the start of 2013',
     '429' : 'Rate limit exceeded',
