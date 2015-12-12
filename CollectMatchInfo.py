20eef9c12cc323c352cc154f7e5fec5f4f87c45d
W-M-T <wgitcommit@gmail.com>
2015-12-12 15:37:39 +0100

    well-formed data test

--- CollectMatchInfo.py
+++ CollectMatchInfo.py
@@ -51,8 +51,31 @@ def main():
     print('')
     api = RiotAPI(api_key)
     matchIDs = list(read_matchIDs('matches.txt'))
-    time.sleep(5)
-    print(api.get_match_by_id(matchIDs[0], {'includeTimeline': False}))
+    #experiment to test if all matches have well-formed data (succesful for test with 800 matches)
+    for match in matchIDs:
+        print(match)
+        temp = api.get_match_by_id(match, {'includeTimeline': False})
+        win100=None
+        win200=None
+        for element in temp['teams']:
+            #print(str(element['teamId'])+" "+str(element['winner']))
+            if element['teamId']==100:
+                win100=element['winner']
+            if element['teamId']==200:
+                win200=element['winner']
+        if (win100==None or win200==None) or (win100==win200):
+            print('ERROR ERROR TEAM ERRROOOOOORR ERROROOROROROROROR ERRORR!!!')
+        count100 = 0
+        count200 = 0
+        for element in temp['participants']:
+            #print(str(element['championId'])+" "+str(element['teamId']))
+            if element['teamId']==100:
+                count100+=1
+            if element['teamId']==200:
+                count200+=1
+        if count100!=5 or count200!=5:
+            print('ERROR ERROR CHAMPION ERRROOOOOORR ERROROOROROROROROR ERRORR!!!')
+        #print('')
     '''
     for i in range(0, parts):
         print("Processing part " + str(i+1) + " out of " + str(parts) + "...")
