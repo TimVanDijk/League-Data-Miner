288e3dbb94539eae9960b34c5626a14b9218daec
W-M-T <wgitcommit@gmail.com>
2016-01-07 20:15:55 +0100

    data validation

--- superStrip.py
+++ superStrip.py
@@ -5,7 +5,7 @@ import os.path
 import sys
 import json
 '''
-def write_matchInfo(outputFile, matchInfo, part):
+def write_matchInfo(outputFile, matchInfo):
     encoder = json.JSONEncoder()
     with open(outputFile, 'w') as database:
             database.write(encoder.encode(matchInfo))
@@ -14,24 +14,68 @@ def write_matchInfo(outputFile, matchInfo, part):
     database.close()
 '''
 def read_matchInfo(inputFile):
-    print("Reading json")
-    print('')
+    print("Reading "+inputFile)
     with open(inputFile, 'r') as database:
             data = json.load(database)
     database.close()
     return data
-'''                
+                
 def strip_info(matchInfo, idSet):
     strippedInfo = []
-    print(len(idSet))
     for matchElement in matchInfo:
+        #check if dupe or null
         if matchElement == None:
-            print('match '+str(matchElement)+' = NONE ERORORORORORORO EROOOOR')
+            print('match '+str(matchElement)+' = None')
             continue
-        if matchElement['matchId'] in idSet:
+        if matchElement['matchId'] not in idSet:
             print("dupe found and destroyed")
             continue
-        idSet.add(matchElement['matchId'])
+        idSet.remove(matchElement['matchId'])
+
+        #check if victory valid
+        win100=None
+        win200=None
+        for team in match['teams']:
+            if team['teamId']==100:
+                win100=team['winner']
+            if team['teamId']==200:
+                win200=team['winner']
+        if (win100==None or win200==None) or (win100==win200):
+            print('match '+str(matchElement)+' victory data incorrect')
+            continue
+        
+        #check if champions and bans valid
+        count100 = 0
+        champions100 = []
+        count200 = 0
+        champions200 = []
+        for participant in match['participants']:
+            if participant['teamId']==100:
+                count100+=1
+                champions100.append(participant['championId'])
+            if participant['teamId']==200:
+                count200+=1
+                champions200.append(participant['championId'])
+                
+        if count100!=5 or count200!=5:
+            print('match '+str(matchElement)+' champion count incorrect')
+            print("team 100: " + str(count100) + " team 200: " + str(count200))
+            for participant in match['participants']:
+                print(participant)
+
+        championstotal = champions100 + champions200
+
+        for champ, amount in Counter(championstotal).items():
+            if (amount > 1):
+                print(Counter(championstotal))
+                print(match['matchId'])
+
+            
+
+
+
+        
+        
         strippedElement = {}
         strippedElement['teams'] = matchElement['teams']
         for item in matchElement['participants']:
@@ -43,10 +87,11 @@ def strip_info(matchInfo, idSet):
         strippedElement['participants'] = matchElement['participants']
         strippedInfo.append(strippedElement)
     return strippedInfo
-'''
+
 def fillIdset(parts, idSet):
     for curIndex in range(parts):
         temp = read_matchInfo("matchInfo_part_"+str(curIndex)+".json")
+        print(matchList[0].keys())
         for match in temp:
             if match != None:
                 idSet.add(match['matchId'])
@@ -55,9 +100,8 @@ def main():
     idSet = set()
     parts = int(input('How many parts need to be processed?'))
     fillIdset(parts, idSet)
-    print(len(idSet))
+    print(len(idSet) + " unique matches found")
     '''
-    parts = 20  
     for curIndex in range(parts):
         print("Start part " + str(curIndex))
         temp = read_matchInfo("matchInfo_part_"+str(curIndex)+".json")
@@ -66,46 +110,8 @@ def main():
         write_matchInfo("clean_match_part_"+str(curIndex)+".json", temp, curIndex)
         print("Done with part " + str(curIndex))
         print("Writing it to disk..")
-        
-        
-    '
-    #experiment to test if all matches have well-formed data (succesful for test with 800 matches)
-    matchIDs = read_matchIDs('matches.txt')
-    for match in matchIDs:
-        print(match)
-        temp = api.get_match_by_id(match, {'includeTimeline': False})
-        win100=None
-        win200=None
-        for element in temp['teams']:
-            print(str(element['teamId'])+" "+str(element['winner']))
-            if element['teamId']==100:
-                win100=element['winner']
-            if element['teamId']==200:
-                win200=element['winner']
-        if (win100==None or win200==None) or (win100==win200):
-            print('ERROR ERROR TEAM ERRROOOOOORR ERROROOROROROROROR ERRORR!!!')
-        count100 = 0
-        count200 = 0
-        for element in temp['participants']:
-            print(str(element['championId'])+" "+str(element['teamId']))
-            if element['teamId']==100:
-                count100+=1
-            if element['teamId']==200:
-                count200+=1
-        if count100!=5 or count200!=5:
-            print('ERROR ERROR CHAMPION ERRROOOOOORR ERROROOROROROROROR ERRORR!!!')
-        print('')
-        time.sleep(20)
     '''
-    '''
-    for i in range(0, parts):
-        print("Processing part " + str(i+1) + " out of " + str(parts) + "...")
-        matchListPart = matchIDs[int(i/parts*len(matchIDs)):int((i+1)/parts*len(matchIDs))]
-        matchInfoList = collect_matchInfo(api, matchListPart)
-        write_matches("matchinfo_part"+str(i)+".txt", matchInfoList)
-        
-    print("Done")
-    '''
+    
 
 if __name__ == "__main__":
     main()
